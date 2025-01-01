8ae3ac798089f6eaa04b05846409218b34451805
drsheko <drsheko91@gmail.com>
2025-01-01 12:21:40 -0600

    ADD API to send emails to portfolio

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,41 +1,67 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var nodemailer = require('nodemailer');
+var nodemailer = require("nodemailer");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
-router.post('/api/email', async(req, res, next)  =>{
+router.post("/api/email", async (req, res, next) => {
   var fullname = req.body.fullName;
   var company = req.body.company;
   var email = req.body.email;
   var message = req.body.message;
-  console.log(process.env.EMAIL)
- 
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user:process.env.EMAIL,
-    pass: process.env.PASSWORD
-  }
-});
-
-var mailToAdmin = {
-  from: process.env.EMAIL,
-  to: process.env.MAINEMAIL,
-  subject: 'J&D Client',
-  text: ` Sender : ${fullname}, 
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+  var mailToAdmin = {
+    from: process.env.EMAIL,
+    to: process.env.MAINEMAIL,
+    subject: "J&D Client",
+    text: ` Sender : ${fullname}, 
           Company : ${company}, 
           Email-Address : ${email}, 
-          Message : ${message}`
-};
-
-transporter.sendMail(mailToAdmin, function(error, info){
-  if (error) {
-     return res.status(500).json({success:false, error})
-  } else {
-   return res.status(200).json({success:true, info})
-  }
+          Message : ${message}`,
+  };
+  transporter.sendMail(mailToAdmin, function (error, info) {
+    if (error) {
+      return res.status(500).json({ success: false, error });
+    } else {
+      return res.status(200).json({ success: true, info });
+    }
+  });
 });
+
+router.post("/api/portfolio/email", async (req, res, next) => {
+  var fullname = req.body.fullName;
+  var email = req.body.email;
+  var message = req.body.message;
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  var mailToAdmin = {
+    from: process.env.EMAIL,
+    to: process.env.EMAIL2,
+    subject: "Shady Portfolio",
+    text: ` Sender : ${fullname}, 
+          Email-Address : ${email}, 
+          Message : ${message}`,
+  };
+
+  transporter.sendMail(mailToAdmin, function (error, info) {
+    if (error) {
+      return res.status(500).json({ success: false, error });
+    } else {
+      return res.status(200).json({ success: true, info });
+    }
+  });
 });
 module.exports = router;
