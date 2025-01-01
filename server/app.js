db1b8bb8926106a1ccd01181d85a904a08534f70
drsheko <drsheko91@gmail.com>
2025-01-01 12:55:32 -0600

    clean up

--- server/app.js
+++ server/app.js
@@ -22,13 +22,6 @@ var mailOptions = {
   text: 'That was easy!'
 };
 
-/*transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});*/
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
